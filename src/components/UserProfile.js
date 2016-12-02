a8855859b50a20a05967f17e129511fa68033e97
Kelvin Tan <kelvin.tyb@gmail.com>
2016-12-02 13:49:38 +0800

    minor fix

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,55 +2,56 @@ import React from 'react';
 import Cat from "./Cat";
 
 class UserProfile extends React.Component {
-
-  this.state = {
-    //
-    user: {
-      uid: 1,
-      fosterList: {
+  constructor(){
+    super();
+    this.state = {
+      //
+      user: {
+        uid: 1,
+        fosterList: {
           cat1: true,
           cat3: true,
           cat4: true
+        }
+      },
+      //cats - note that age is going to be in terms of months
+      cats: {
+        cat1: {
+          name: "Kinder",
+          color: "tabby",
+          age: "1",
+          isForAdoption: true,
+          imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
+          user_id: 1
+        },
+        cat2: {
+          name: "NotByUser",
+          color: "white",
+          age: "3",
+          isForAdoption: true,
+          imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
+          user_id: 2
+        },
+        cat3: {
+          name: "NotAdopted",
+          color: "others",
+          age: "6",
+          isForAdoption: false,
+          imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
+          user_id: 1
+        },
+        cat4: {
+          name: "Clov",
+          color: "Calico",
+          age: "13",
+          isForAdoption: false,
+          imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
+          user_id: 1
+        }
       }
     }
-    //cats - note that age is going to be in terms of months
-    cats: {
-      cat1: {
-        name: "Kinder",
-        color: "tabby",
-        age: "1",
-        isForAdoption: true,
-        imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
-        user_id: 1
-      },
-      cat2: {
-        name: "NotByUser",
-        color: "white",
-        age: "3",
-        isForAdoption: true,
-        imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
-        user_id: 2
-      },
-      cat3: {
-        name: "NotAdopted",
-        color: "others",
-        age: "6",
-        isForAdoption: false,
-        imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
-        user_id: 1
-      },
-      cat4: {
-        name: "Clov",
-        color: "Calico",
-        age: "13",
-        isForAdoption: false,
-        imageUrl: "http://www.catwelfare.org/sites/default/files/imagecache/adpt_thumbnail_large/adoptions/kinder5.jpg",
-        user_id: 1
-      }
   }
   //display cats for which isForAdoption == true
-
-
   render() {
     return (
       <div>
