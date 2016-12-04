e49d259198ec7a7babed05241bf10ee79deb59f6
Kelvin Tan <kelvin.tyb@gmail.com>
2016-12-04 20:15:49 +0800

    fixed router bug by reverting to older react-router

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,1 +1,28 @@
 //this file for refactoring away routes inside index.js, if needed
+import React from 'react';
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
+
+import App from '../App';
+import About from "../components/About";
+import Home from "../components/Home";
+import UserProfile from "../components/UserProfile"
+import CatIndex from "../components/CatIndex";
+import Placeholder from "../components/Placeholder"
+
+const routes = (
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/cats" component={CatIndex}/>
+        <Route path="/chats/:chatId" component={Placeholder}/>
+        <Route path="/profile" component={UserProfile}>
+          <Route path="/profile/fosterlist" component={Placeholder}/>
+          <Route path="/profile/likelist" component={Placeholder}/>
+          <Route path="/profile/chats" component={Placeholder}/>
+        </Route>
+      </Route>
+    </Router>
+)
+
+export default routes;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
+// import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import './css/index.css';
+import routes from "./config/routes"
+import App from './App'; //if routes stop working, use app component for testings
 
-import App from './App';
-import About from "./components/About";
-import Home from "./components/Home";
-import UserProfile from "./components/UserProfile"
-import CatIndex from "./components/CatIndex";
-import Placeholder from "./components/Placeholder"
-
-render((
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/cats" component={CatIndex}/>
-        <Route path="/chats/:chatId" component={Placeholder}/>
-        <Route path="/profile" component={UserProfile}>
-          <Route path="/profile/fosterlist" component={Placeholder}/>
-          <Route path="/profile/likelist" component={Placeholder}/>
-          <Route path="/profile/chats" component={Placeholder}/>
-        </Route>
-      </Route>
-    </Router>
-), document.getElementById('root'))
+render( routes, document.getElementById('root'))
